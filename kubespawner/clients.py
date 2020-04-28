f98b54f00cce4a3994caf57219739ec912bbab9b
xcheng1 <xcheng1@kpmg.com>
2020-04-27 18:33:04 -0700

    use load_incluster_config to refresh token

--- kubespawner/clients.py
+++ kubespawner/clients.py
@@ -7,8 +7,9 @@ each of which spawns an unused max-size thread pool
 from unittest.mock import Mock
 import weakref
 
-import kubernetes.client
-from kubernetes.client import api_client
+# import kubernetes.client
+# from kubernetes.client import api_client
+from kubernetes import client, config
 
 # FIXME: remove when instantiating a kubernetes client
 # doesn't create N-CPUs threads unconditionally.
@@ -17,9 +18,9 @@ from kubernetes.client import api_client
 # This is known to work for kubernetes-4.0
 # and may need updating with later kubernetes clients
 _dummy_pool = Mock()
-api_client.ThreadPool = lambda *args, **kwargs: _dummy_pool
+# api_client.ThreadPool = lambda *args, **kwargs: _dummy_pool
 
-_client_cache = {}
+# _client_cache = {}
 
 
 def shared_client(ClientType, *args, **kwargs):
@@ -30,18 +31,21 @@ def shared_client(ClientType, *args, **kwargs):
     will all return the same instance until
     all references to the client are cleared.
     """
-    kwarg_key = tuple((key, kwargs[key]) for key in sorted(kwargs))
+    # kwarg_key = tuple((key, kwargs[key]) for key in sorted(kwargs))
     # cache_key = (ClientType, args, kwarg_key)
-    Client = getattr(kubernetes.client, ClientType)
-    client = Client(*args, **kwargs)
+    # Client = getattr(kubernetes.client, ClientType)
+    # client = Client(*args, **kwargs)
     # if cache_key in _client_cache:
     #     # resolve cached weakref
     #     # client can still be None after this!
     #     client = _client_cache[cache_key]()
+    # current way does not load refreshed token
+    config.load_incluster_config()
+    v1 = client.CoreV1Api()
 
     # if client is None:
     #     Client = getattr(kubernetes.client, ClientType)
     #     client = Client(*args, **kwargs)
     #     # cache weakref so that clients can be garbage collected
     #     _client_cache[cache_key] = weakref.ref(client)
-    return client
+    return v1
